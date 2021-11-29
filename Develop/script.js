508b18fe9ae03a5e09b455d9443e001c662b88f0
iimkenny <kennywash1015@gmail.com>
2021-11-28 18:37:50 -0800

    added length and charType

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,26 @@
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
 // Assignment code here
 
-
+// length of password (has to be between 8-128 characters).
+var length = Number(prompt("How many character would you like your password to be? Please type a number between 8 and 128"));
+console.log(length);
+// which characters
+var charType = prompt("Which character type would you like: uppercase, lowercase, numeric, special?");
+console.log(charType);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
